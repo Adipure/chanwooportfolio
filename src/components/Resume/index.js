0089947c001977a6099942f7f8fee2bc18999c74
Chanwoo Hwang <Chanwoo1017@hotmail.com>
2022-01-31 20:35:19 -0800

    update resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,31 +1,31 @@
 import React from 'react';
+import myResume from '../myResume/myResume.pdf';
 
 function Resume() {
   return (
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+         <myResume/>
         </p>
-        <h3>Front-end Proficiencies</h3>
+        <h3>Skills</h3>
+        <h3>Front-end</h3>
         <ul className="skills">
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
           <li>jQuery</li>
-          <li>responsive design</li>
+          <li>Responsive Design</li>
           <li>React</li>
           <li>Bootstrap</li>
         </ul>
-        <h3>Back-end Proficiencies</h3>
+        <h3>Back-end </h3>
         <ul className="skills">
           <li>APIs</li>
           <li>Node</li>
           <li>Express</li>
           <li>MySQL, Sequelize</li>
           <li>MongoDB, Mongoose</li>
-          <li>REST</li>
-          <li>GraphQL</li>
         </ul>
       </div>
     </section>
